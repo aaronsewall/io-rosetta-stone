6664f3f74f67f7e39a0008f7ae68eceaa6217148
Aaron Sewall <aaron@tvisioninsights.com>
2020-01-08 15:39:16 -0500

    Use TypeVar S to represent additional typevar for bind and _and

--- python/app.py
+++ python/app.py
@@ -3,41 +3,41 @@ from typing import Optional, Callable
 import io_
 
 
-def app_logic(x: Optional[str], y: Optional[str]) -> str:
-    assert isinstance(x, str)
-    assert isinstance(y, str)
+def app_logic(x: str, y: str) -> str:
     return "Result: " + str(int(x) + int(y))
 
 
-def print_maybe(str_m: Optional[str]) -> io_.IO[str]:
+def print_maybe(str_m: Optional[str]) -> io_.IO[None]:
     return io_.put_str_ln(str_m) if str_m is not None else io_._return(None)
 
 
-def prompt(greet: Optional[str], confirm: Callable[[Optional[str]], str]) -> io_.IO[str]:
+def prompt(greet: Optional[str], confirm: Callable[[str], str]) -> io_.IO[str]:
     salute = print_maybe(greet)._and(io_.get_line)
 
-    def certify(l: Optional[str]) -> io_.IO[str]:
+    def certify(l: str) -> io_.IO[str]:
         return print_maybe(confirm(l))._and(io_._return(l))
 
     return salute.bind(certify)
 
 
-def first_number(l: Optional[str]) -> str:
+def first_number(l: str) -> str:
     return "Got first input: " + str(l)
 
 
-def second_number(l: Optional[str]) -> str:
+def second_number(l: str) -> str:
     return "Got second input: " + str(l)
 
 
-def input_second_number_and_print_result(x: Optional[str]) -> io_.IO[str]:
-    def print_result(y: Optional[str]) -> io_.IO[str]:
+def input_second_number_and_print_result(x: str) -> io_.IO[None]:
+    def print_result(y: str) -> io_.IO[None]:
         return io_.put_str_ln(app_logic(x, y))
 
     return prompt(None, second_number).bind(print_result)
 
 
-app = prompt("Please input two numbers.", first_number).bind(input_second_number_and_print_result)
+app = prompt("Please input two numbers.", first_number).bind(
+    input_second_number_and_print_result
+)
 
 if __name__ == "__main__":
     app.unsafeRunIO()
--- python/io_.py
+++ python/io_.py
@@ -1,34 +1,34 @@
-from typing import Callable, Optional, TypeVar, Generic
+from typing import Callable, TypeVar, Generic
 
 T = TypeVar("T")
+S = TypeVar("S")
 
 
 class IO(Generic[T]):
-    def __init__(self, run: Callable[[], Optional[T]]) -> None:
+    def __init__(self: "IO[T]", run: Callable[..., T]) -> None:
         self.unsafeRunIO = run
 
-    def map(self, f: Callable[[Optional[T]], Optional[T]]) -> "IO[T]":
+    def map(self: "IO[T]", f: Callable[..., T]) -> "IO[T]":
         return IO(lambda: f(self.unsafeRunIO()))
 
-    def bind(self, f: Callable[[Optional[T]], "IO[T]"]) -> "IO[T]":
+    def bind(self: "IO[T]", f: Callable[..., "IO[S]"]) -> "IO[S]":
         return IO(lambda: f(self.unsafeRunIO()).unsafeRunIO())
 
-    def _and(self, other: "IO[T]") -> "IO[T]":
-        def go() -> Optional[T]:
+    def _and(self: "IO[T]", other: "IO[S]") -> "IO[S]":
+        def go() -> S:
             self.unsafeRunIO()
             return other.unsafeRunIO()
 
         return IO(go)
 
 
-def _return(x: Optional[T]) -> IO[T]:
+def _return(x: T) -> IO[T]:
     return IO(lambda: x)
 
 
-def put_str_ln(s: T) -> IO[T]:
+def put_str_ln(s: str) -> IO[None]:
     def go() -> None:
         print(s)
-        return None
 
     return IO(go)
 
